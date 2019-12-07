192b04afe57e3c1ececad6ecb8d80a60abc2090c
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-07 17:35:43 +0530

    Added: Stored result in dictionary

--- ArithmeticOperation.sh
+++ ArithmeticOperation.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
 
+declare -A  resultDict
+
 read -p "Enter the first number: " a
 read -p "Enter the second number: " b
 read -p "Enter the third number: " c
 
-echo $(( $a + $b * $c ))
-echo $(( $a * $b + $c ))
-echo $(( $c + $a / $b ))
-echo $(( $a % $b + $c ))
+resultDict["$a + $b * $c"]=$(( $a + $b * $c ))
+resultDict["$a * $b + $c"]=$(( $a * $b + $c ))
+resultDict["$c + $a / $b"]=$(( $c + $a / $b ))
+resultDict["$a % $b + $c"]=$(( $a % $b + $c ))
+
