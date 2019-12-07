2a4d9dd1a5a6c5317901a07d36e3ce7650622252
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-07 18:37:32 +0530

    Added: Arranging array in descending

--- ArithmeticOperation.sh
+++ ArithmeticOperation.sh
@@ -11,7 +11,22 @@ resultDict[Operation2]=$(( $a * $b + $c ))
 resultDict[Operation3]=$(( $c + $a / $b ))
 resultDict[Operation4]=$(( $a % $b + $c ))
 
+count=0
+
 for result in "${!resultDict[@]}"
 do
 	resultArr[((count++))]="${resultDict[$result]}"
+done
+
+for((j=0; j<4;j++))
+do
+	for((i=0;i<4-$j-1;i++))
+	do
+		if [ ${resultArr[$i]} -lt ${resultArr[$(($i +1))]} ]
+		then
+			temp=${resultArr[$i]}
+			resultArr[(($i))]=${resultArr[$(( $i +1 ))]}
+			resultArr[$(( $i +1 ))]=$temp
+		fi
+	done
 done
