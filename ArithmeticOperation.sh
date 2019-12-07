734e0348eeb7c231e6047ebc049e8c7cb21a7a8b
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-07 17:51:11 +0530

    Added: Reading value of dictionary to array

--- ArithmeticOperation.sh
+++ ArithmeticOperation.sh
@@ -6,8 +6,12 @@ read -p "Enter the first number: " a
 read -p "Enter the second number: " b
 read -p "Enter the third number: " c
 
-resultDict["$a + $b * $c"]=$(( $a + $b * $c ))
-resultDict["$a * $b + $c"]=$(( $a * $b + $c ))
-resultDict["$c + $a / $b"]=$(( $c + $a / $b ))
-resultDict["$a % $b + $c"]=$(( $a % $b + $c ))
+resultDict[Operation1]=$(( $a + $b * $c ))
+resultDict[Operation2]=$(( $a * $b + $c ))
+resultDict[Operation3]=$(( $c + $a / $b ))
+resultDict[Operation4]=$(( $a % $b + $c ))
 
+for result in "${!resultDict[@]}"
+do
+	resultArr[((count++))]="${resultDict[$result]}"
+done
