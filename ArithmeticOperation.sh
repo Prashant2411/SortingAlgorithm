cc33194812a6c6a9b26376022416bde3c2fda377
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-12 11:09:06 +0530

    Modified: Added function for ascending and descending order

--- ArithmeticOperation.sh
+++ ArithmeticOperation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+count=0
+
 declare -A  resultDict
 
 read -p "Enter the first number: " a
@@ -11,35 +13,47 @@ resultDict[Operation2]=$(( $a * $b + $c ))
 resultDict[Operation3]=$(( $c + $a / $b ))
 resultDict[Operation4]=$(( $a % $b + $c ))
 
-count=0
-
-for result in "${!resultDict[@]}"
-do
-	resultArr[((count++))]="${resultDict[$result]}"
-done
-
-for((j=0; j<4;j++))
-do
-	for((i=0;i<4-$j-1;i++))
+function getDesc () {
+	for((j=0; j<4;j++))
 	do
-		if [ ${resultArr[$i]} -lt ${resultArr[$(($i +1))]} ]
-		then
-			temp=${resultArr[$i]}
-			resultArr[(($i))]=${resultArr[$(( $i +1 ))]}
-			resultArr[$(( $i +1 ))]=$temp
-		fi
+		for((i=0;i<4-$j-1;i++))
+		do
+			if [ ${resultArr[$i]} -lt ${resultArr[$(($i +1))]} ]
+			then
+				temp=${resultArr[$i]}
+				resultArr[(($i))]=${resultArr[$(( $i +1 ))]}
+				resultArr[$(( $i +1 ))]=$temp
+			fi
+		done
 	done
-done
+}
 
-for((j=0; j<4;j++))
+function getAsc () {
+	for((j=0; j<4;j++))
 	do
-	for((i=0;i<4-$j-1;i++))
-	do
-		if [ ${array[$i]} -gt ${array[$(($i +1))]} ]
-		then
-			temp=${array[$i]}
-         		array[(($i))]=${array[$(( $i +1 ))]}
-         		array[$(( $i +1 ))]=$temp
-	 	fi
+		for((i=0;i<4-$j-1;i++))
+		do
+			if [ ${resultArr[$i]} -gt ${resultArr[$(($i +1))]} ]
+			then
+				temp=${resultArr[$i]}
+				resultArr[(($i))]=${resultArr[$(( $i +1 ))]}
+				resultArr[$(( $i +1 ))]=$temp
+	 		fi
+		done
 	done
-done
+}
+
+function main () {
+	for result in "${!resultDict[@]}"
+	do
+		resultArr[((count++))]="${resultDict[$result]}"
+	done
+	getDesc
+	echo "Descending order: "
+	echo "${resultArr[@]}"
+	getAsc
+	echo "Ascending Order: "
+	echo "${resultArr[@]}"
+}
+
+main
